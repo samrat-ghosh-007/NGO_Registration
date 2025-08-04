4f35a57bcecc232ba08a18f425da0666b6877895
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2025-08-04 11:22:00 +0530

    Fix: added proper email validation in NGO model

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, validate: {
+    validator: (value) => validator.isEmail(value),
+    message: 'Invalid email address'
+  } 
+},
   password: String,
   role: { type: String, enum: ['volunteer', 'intern', 'admin'], default: 'volunteer' }
 });
